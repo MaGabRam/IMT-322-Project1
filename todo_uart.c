bc6d04eba04444dcc9f81b34cb75c21db9ddb0d1
MaGabRam <136017727+MaGabRam@users.noreply.github.com>
2025-03-28 11:31:05 -0400

    Update todo_uart.c

--- todo_uart.c
+++ todo_uart.c
@@ -4,7 +4,7 @@
 #include "driverlib/debug.h"
 #include "driverlib/gpio.h"
 #include "driverlib/sysctl.h"
-#include "driverlib/uart.h"
+#include "driverlib/uart.h"  # esto es la libreria para el uart
 #include "driverlib/timer.h"
 #include "driverlib/interrupt.h"
 #include "driverlib/pin_map.h"
@@ -21,14 +21,14 @@ uint32_t FS=120000000*1.5; //120 MHz
 void timer0A_handler(void);
 uint8_t switch_state=0;
 char  msg[]="h";
-char data[100];
+char data[100]; // solo hemos usado esto
 char c[100];
 int aux=0;
 #define TIMEOUT 1000000 
 int
 main(void)
 {
-    
+    //puertos
     SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480), 120000000);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
@@ -56,15 +56,16 @@ main(void)
     // TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
     // // Enable the timer
     // TimerEnable(TIMER0_BASE, TIMER_A);
-  //UART
+  //UART--------------------------------------------------------------------------------------------------------------------------
+    //configurar puertos y periferico
     SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-
-    GPIOPinConfigure(GPIO_PA0_U0RX);
-    GPIOPinConfigure(GPIO_PA1_U0TX);
+    // de esta manera funciona con usb
+    GPIOPinConfigure(GPIO_PA0_U0RX); //de recepcion
+    GPIOPinConfigure(GPIO_PA1_U0TX); //de transimision
     GPIOPinTypeUART(GPIO_PORTA_BASE, 0x03);
 
-    UARTStdioConfig(0, 9600, 120000000);
+    UARTStdioConfig(0, 9600, 120000000); // simepre se pone 0, 9600 son los baudios, frecuencia
     
     while(1)
     {   
@@ -72,7 +73,8 @@ main(void)
         // Changing the timer delay
         if (buttonState == 0x00 ) 
         {
-            UARTprintf("motor1\n");
+            UARTprintf("motor1\n"); //para enviar datos por el uart y siempre se poner /n para que la otra placa detecte que es el final del mensaje... esto lpo recibe la rasp
+            // tambien se envian nuemeros pero esto se convertira a texto 
         }
         uint8_t buttonState2 = GPIOPinRead(GPIO_PORTJ_BASE, 0x02);
         // Changing the timer delay
@@ -81,12 +83,12 @@ main(void)
             UARTprintf("motor2\n");
 
         }
-        while(UARTCharsAvail(UART0_BASE)){
-            UARTgets(data,100);
-            if(strcmp(data, "buzzer") == 0){
+        while(UARTCharsAvail(UART0_BASE)){  // si esta habilitabo el uart
+            UARTgets(data,100); // se almacenan los datos en esta cadena, si es que no se reciben acaracteres , esto se bloquea por eso se pone el while en el casa de que se haya recibido un mensaje
+            if(strcmp(data, "buzzer") == 0){ // compara la cadena con el dato o señal enviada
                 aux=1;
-               // SysCtlDelay(80000000);
-                break;
+               // SysCtlDelay(80000000); // si se ponia aqui el delay habia un error, por eso auxiliar = 1
+                break; 
             }
             else{
                 aux=0;
@@ -96,7 +98,7 @@ main(void)
         }
         if(aux==1){
             GPIOPinWrite(GPIO_PORTF_BASE, 0x03, 0x03); 
-            SysCtlDelay(80000000);
+            SysCtlDelay(80000000); // 120M/3 *cantidad de delay // entre envio y recepcion de datos hay pequeña espera sino, se bloquea  
             GPIOPinWrite(GPIO_PORTF_BASE, 0x03, 0x00);
                }
         
