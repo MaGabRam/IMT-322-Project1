c7df4b5e434b769914e536472bc93179bc530cda
MaGabRam <136017727+MaGabRam@users.noreply.github.com>
2025-03-28 12:03:46 -0400

    Update ssh_final.py

--- ssh_final.py
+++ ssh_final.py
@@ -1,6 +1,11 @@
+# esto esperamoa que ya no se desconfigure
+# raspi -config  -> abre interfaz para habilitar o desahilitar, se habilita lo de USB
+
+
 import RPi.GPIO as GPIO
 import time
-import serial
+import serial # LIBRERIA PARA COMUNICACION SERIAL
+
 button1_pin = 19  
 aux=3
 #motores
@@ -23,6 +28,7 @@ GPIO.setup(button1_pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup([IN1, IN2, IN3, IN4, ENA, ENB], GPIO.OUT)
 
 # PWM (frecuencia 1000Hz)
+# esto es una funcion, debe tener formula, directamente lo convierte Hz
 pwmA = GPIO.PWM(ENA, 1000)
 pwmB = GPIO.PWM(ENB, 1000)
 
@@ -53,19 +59,23 @@ def read_interval():
         except ValueError:
             print(f"⚠️ Valor inválido: '{line}', usando 50.")
             return 50
-#UART
-ser = serial.Serial('/dev/ttyACM0', 9600)
-ser.reset_input_buffer()
+#UART---------------------------------------------------------------------------------------------------------------------
+
+#la comunicacion serial se vuelve una variable y poner puerto deonde se conecta la TIVA
+#SE PONE sudo ls /dev/tty Y deberia salir en nombre, puede cambiar entonces hay que revisar is es que no compila: no se encuentra el puerto
+ser = serial.Serial('/dev/ttyACM0', 9600) # con la misma velocidade de baudios
+ser.reset_input_buffer() # esto no entiendo pero maso como que limpia lo que habia
 
 print("Escuchando...")
 msg="buzzer"
 try:
     while True:
-        duty=read_interval()
+        # el uart es esto---------------------------------------------------------------
+        duty=read_interval() # si se recibe algo se ejecuta lo siguiente
         if ser.in_waiting > 0:
-            raw = ser.readline()
-            try:
-                data = raw.decode('utf-8').rstrip()
+            raw = ser.readline() # aqui lee todo la linea
+            try: 
+                data = raw.decode('utf-8').rstrip() # utf-8 es un formato de codificacion : universal algo # rstrip limipa la linea
                 print("Tiva dice:", data)
                 if data == "motor1":
                     print(duty)
@@ -73,10 +83,13 @@ try:
                 elif data == "motor2":
                     print(duty)
                     motorB_forward(duty)
+        # hasta aqui uart ---------------------------------------------------------------
             except UnicodeDecodeError:
                 print("⚠️ Datos corruptos ignorados:", raw)
+        # s eenvian datos desde el uart desde la rasp:
         if GPIO.input(button1_pin) == GPIO.LOW:
             print("Button 1 on")
+            # este se ejecuta solo una vez  por eso mas abajo se pone off
             ser.write("buzzer\n".encode('utf-8'))  # Enviar a Tiva
             aux=1
             time.sleep(0.2)
